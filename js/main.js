8f1e61621a0a6af0274e1c38a17f20930848e688
Owen De Waele <owendewa@student.arteveldehs.be>
2020-03-18 21:52:09 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const videos = [
     
     {
         title: 'Intro',
-        img: 'images/mascot/Intro.png',
+        img: 'images/mascot/intro.png',
         src: 'https://player.vimeo.com/video/395416981'
     },
     {
